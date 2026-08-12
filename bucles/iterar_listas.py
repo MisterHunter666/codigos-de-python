0df1451c87024135f7cf988052eb3655f8a5d407
MisterHunter666 <marcos93bolonese@gmail.com>
2026-08-12 03:39:13 -0300

    Actualizando iterar_listas.py

--- bucles/iterar_listas.py
+++ bucles/iterar_listas.py
@@ -29,20 +29,94 @@
 # Las listas ya vienen definidas para iterarse de esa forma
 # Las cadenas de texto también se pueden iterar, ya vamos a ver cómo, los diccionarios, los conjuntos...
 # Así que, vamos a arrancar en todo este mundo y vamos a empezar
-# Para comenzar tenemos justamente el primer bucle, el primer tipo de bucle que es FOR,
-# Que es el más famoso de todos los bucles, el bucle FOR
-# Y FOR es un bucle que nos permite iterar, crear una iteración
-# Así que, vamos a arrancar a trabajar con FOR
-# Antes de comenzar a iterar listas y usar el bucle FOR, de hecho, podemos arrancar ya mismo, entendamos bien a fondo el concepto de FOR
-# Mejor dicho, el funcionamiento de FOR
-# En Python, los bucles son: FOR, IN
-# En otros lenguajes, tenemos el FOR que tiene tres condiciones
+# Para comenzar tenemos justamente el primer bucle, el primer tipo de bucle que es for,
+# Que es el más famoso de todos los bucles, el bucle for
+# Y for es un bucle que nos permite iterar, crear una iteración
+# Así que, vamos a arrancar a trabajar con for
+# Antes de comenzar a iterar listas y usar el bucle for, de hecho, podemos arrancar ya mismo, entendamos bien a fondo el concepto de for
+# Mejor dicho, el funcionamiento de for
+# En Python, los bucles son: for, in
+# En otros lenguajes, tenemos el for que tiene tres condiciones
 # O sea, i=0, mientras que i<".", i++, o i--
 # Esa famosa forma de hacer bucles
 # Python lo simplifica completamente
-# Y Python usa: FOR, IN
-# ¿Qué es un FOR, IN?
+# Y Python usa: for, in
+# ¿Qué es un for, in?
 # Bien
-# El "FOR IN" es un bucle que justamente lo que hace es crear una variable que en cada vuelta va a ser igual a ese pedacito de variable que estamos igualando
+# El "for in" es un bucle que justamente lo que hace es crear una variable que en cada vuelta va a ser igual a ese pedacito de variable que estamos igualando
 # Ok, vamos a pensarnos mejor
 # Vamos ahora a verlo...
+# Ok, supongamos que tenemos una lista "animales"
+# Que va a ser igual a (=) "perro", "gato", "loro" y "cocodrilo"
+# animales = ["perro","gato","loro","cocodrilo"]
+#                |      |      |       |
+#                v      v      v       v
+#                0      1      2       3
+# Una lista con cuatro (4, de 0 a 3) elementos
+# Bastante simple hasta ahora
+# Ahora, ¿cómo iteramos esta lista con for?
+# Simple
+# Primero decimos: 
+# for animal in animales
+# Ponemos los dos puntitos (:)...
+# for animal in animales:
+# Y ahora podemos ejecutar el código
+# La pregunta es, ¿a qué va a ser igual "animal"?
+# ¿Qué valor tiene "animal"?
+# Fácil
+# La primera vez que se ejecuta el código, "animal" va a ser igual (=) al primer elemento (0), es decir, "perro"
+# De hecho, si usamos un "print(animal)" para mostrar qué es lo que tiene la variable "animal" adentro,
+# nos va a decir "perro"
+# perro
+# Cuando termina de dar la vuelta al bloque, va a revisar si hay más elementos en esta lista
+# Si hay más elementos, entonces se vuelve a ejecutar
+# Pero ahora, "animal" es igual (=) al siguiente elemento,
+# o sea, a "gato"
+# O sea que en esta vuelta nos va a mostrar "gato"
+# perro
+# gato
+# Termina de ejecutarse, vuelve a preguntarle si hay elementos
+# y está el "loro"
+# Entonces, en la siguiente vuelta, "animal" es igual a (=) "loro"
+# perro
+# gato
+# loro
+# Nuevamente, termina la ejecución y pregunta si hay otro animal
+# Y sí, está el "cocodrilo"
+# Entonces, ejecuta de vuelta y ahora "animal" es igual a (=) cocodrilo
+# perro
+# gato
+# loro
+# cocodrilo
+# Ahora vuelve a preguntar y ya no hay más animales
+# Entonces, el bucle termina de ejecutarse
+# perro
+# gato
+# loro
+# cocodrilo
+# Y ahora sí, pasamos a la siguiente instrucción
+# Entonces en definitiva, el programa funcionaría de la siguiente forma:
+# Creamos la lista...
+# animales = ["perro","gato","loro","cocodrilo"]
+# Después, ejecuta el bucle tantas veces como animales tenga...
+# for animal in animales:
+# print(animal)
+# perro
+# gato
+# loro
+# cocodrilo
+# Y cuando no tenga más, el programa continúa con su flujo natural
+# Es bastante fácil
+# Vamos a llevarlo ahora a la práctica, iterando una lista
+# Que por eso creamos ese elemento para iterar listas
+# Vamos a iterar de todo un poco
+# Vamos a iterar cadenas de texto, vamos a iterar diccionarios... 
+# Vamos a arrancar iterando listas
+# Vamos a arrancar a escribir el código
+# Para empezar es sencillo
+# Vamos a crear una lista que se llama:
+# lista = animales
+# que era como lo teníamos
+# Y vamos a crear un "gato", un "perro", un "loro" y un "cocodrilo"
+# lista = animales ["perro","gato","loro","cocodrilo"]
+# Bien
